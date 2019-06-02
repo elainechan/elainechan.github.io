80d7acf8a1086c4ab024e8a0a578d84c9d97c96a
elainechan <elaine.chan@outlook.com>
2019-06-02 01:41:24 -0400

    Adds new post.

--- src/templates/single-tag-index.js
+++ src/templates/single-tag-index.js
@@ -1,32 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Header from '../components/Header'
 
 const SingleTagTemplate = (props) => {
   console.log(props)
   const { posts, tag } = props.pageContext
 
   return (
-    <div style={{ fontFamily: 'helvetica' }}>
-      <div>
-        <h2>
-        Posts about {`${tag}`}
-        </h2>
-      </div>
+    <div>
+      <Header />
+      <div style={{ fontFamily: 'helvetica' }}>
         <div>
-          <ul style={{ listStyleType: 'none' }}>
-            {
-              posts.map((post, index) => {
-                return (
-                  <li key={index}>
-                    <Link to={post.frontmatter.path}>
-                      {post.frontmatter.title}
-                    </Link>
-                  </li>
-                )
-              })
-            }
-          </ul>
+          <h2>
+          Posts about {`${tag}`}
+          </h2>
         </div>
+          <div>
+            <ul style={{ listStyleType: 'none' }}>
+              {
+                posts.map((post, index) => {
+                  return (
+                    <li key={index}>
+                      <Link to={post.frontmatter.path}>
+                        {post.frontmatter.title}
+                      </Link>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+      </div>
     </div>
   )
 }
